de8841dfa03fb70bb7a444617ce9573a6b6e8209
Arthur <arthur_2335@hotmail.com>
2022-11-10 21:46:03 -0500

    moved drawing styling to css

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,80 +1,35 @@
-const HEAD = (
-  <div
-    style={{
-      width: "50px",
-      height: "50px",
-      borderRadius: "100%",
-      border: "10px solid black",
-      position: "absolute",
-      top: "50px",
-      right: "-30px",
-    }}
-  />
-);
-const BODY = (
-  <div
-    style={{
-      width: "10px",
-      height: "100px",
-      background: "black",
-      position: "absolute",
-      top: "120px",
-      right: 0,
-    }}
-  />
-);
+import "./HangmanDrawing.css";
+
+const HEAD = <div className="HEAD" />;
+const BODY = <div className="BODY" />;
 const RIGHT_ARM = (
   <div
+    className="RIGHT_ARM"
     style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "150px",
-      right: "-100px",
-      rotate: "-30deg",
       transformOrigin: "left bottom",
     }}
   />
 );
 const LEFT_ARM = (
   <div
+    className="LEFT_ARM"
     style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "150px",
-      right: "10px",
-      rotate: "30deg",
       transformOrigin: "right bottom",
     }}
   />
 );
 const RIGHT_LEG = (
   <div
+    className="RIGHT_LEG"
     style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: "-90px",
-      rotate: "60deg",
       transformOrigin: "left bottom",
     }}
   />
 );
 const LEFT_LEG = (
   <div
+    className="LEFT_LEG"
     style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: 0,
-      rotate: "-60deg",
       transformOrigin: "right bottom",
     }}
   />
@@ -88,36 +43,12 @@ type HangManDrawingProps = {
 
 export function HangmanDrawing({ numberOfGuesses }: HangManDrawingProps) {
   return (
-    <div style={{ position: "relative" }}>
+    <div className="HANGMAN_CONTAINER">
       {BODY_PARTS.slice(0, numberOfGuesses)}
-      <div
-        style={{
-          height: "50px",
-          width: "10px",
-          background: "black",
-          marginLeft: "120px",
-          position: "absolute",
-          top: 0,
-          right: 0,
-        }}
-      />
-      <div
-        style={{
-          height: "10px",
-          width: "200px",
-          background: "black",
-          marginLeft: "120px",
-        }}
-      />
-      <div
-        style={{
-          height: "400px",
-          width: "10px",
-          background: "black",
-          marginLeft: "120px",
-        }}
-      />
-      <div style={{ height: "10px", width: "250px", background: "black" }} />
+      <div className="ROPE" />
+      <div className="SUPPORT_BEAM" />
+      <div className="MAIN_BEAM" />
+      <div className="FLOOR_SUPPORT" />
     </div>
   );
 }
